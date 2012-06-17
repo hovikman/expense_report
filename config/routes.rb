15d345ddf689adcaf1b0cb3ad0ea73f131ee2f6b
Hovik Manvelyan <hovik_manvelyan2002@yahoo.com>
2012-06-17 14:40:37 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 ExpenseReport::Application.routes.draw do
+  root to: 'static_pages#home'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
   resources :expense_details
-
   resources :expenses
-
   resources :expense_types
-
   resources :users
-
   resources :companies
-
   resources :currencies
-
   resources :expense_statuses
-
   resources :user_types
 
   # The priority is based upon order of creation:
@@ -61,10 +60,6 @@ ExpenseReport::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
