5a7344a41fd44fe2fcf58e6f311f9cc0206e04e8
Hovik Manvelyan <hovik_manvelyan2002@yahoo.com>
2012-09-08 23:40:27 -0700

    show currency code and name in currency selector

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -20,10 +20,13 @@ class Currency < ActiveRecord::Base
     def ensure_not_referenced_by_any_company
       raise "Cannot delete currency '#{name}'. There are companies referencing this currency." unless companies.empty?
     end
-
+  
     # ensure that there are no expense_details referencing this currency
     def ensure_not_referenced_by_any_expense_detail
       raise "Cannot delete currency '#{name}'. There are expens details referencing this currency." unless expense_details.empty?
     end
-
+  
+    def code_and_name
+      code + ' ' + name
+    end
 end
