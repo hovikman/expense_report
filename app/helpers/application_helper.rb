91c1680eeec3ded8760d756278e976d6194c526a
Hovik Manvelyan <hovik_manvelyan2002@yahoo.com>
2012-09-12 10:52:18 -0700

    remove duplicate headers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "ExpenseReport Application"
+    base_title = "Expense Report"
     if page_title.empty?
       base_title
     else
