584a5cfb990aebab3a9717c98f42f313c74cdf7f
Hovik Manvelyan <hovik_manvelyan2002@yahoo.com>
2012-09-13 19:58:03 -0700

    use smtp_tls plugin to send emails from gmail server

--- config/environment.rb
+++ config/environment.rb
@@ -5,16 +5,5 @@ require File.expand_path('../application', __FILE__)
 ExpenseReport::Application.initialize!
 
 ExpenseReport::Application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "www.gmail.com",
-    :authentication       => "plain",
-    :user_name            => "hovikman@gmail.com",
-    :password             => "hmhsr000",
-    :enable_starttls_auto => true
-  }
-end    
+  config.action_mailer.raise_delivery_errors = true
+end
