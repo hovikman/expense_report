b2a14f383f1cf9200e086310e1743612294af43c
hovikman <hovikman@gmail.com>
2012-09-23 21:56:38 -0700

    fix problem with 'Remember me' check_box

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      sign_in(user, params[:session][:remember_me])
+      sign_in(user, params[:session][:remember_me] == '1')
       redirect_to root_url, notice: "Signed in!"
     else
       flash.now[:error] = 'Invalid email/password combination'
