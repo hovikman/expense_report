c931a78c75e26af88acd8f112eb28e66c798aac4
hovikman <hovikman@gmail.com>
2012-10-03 09:11:37 -0700

    remove test directory

--- test/unit/helpers/expense_details_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ExpenseDetailsHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/expense_statuses_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ExpenseStatusesHelperTest < ActionView::TestCase
-end
